48d4ef5ebc1e86bc3c643a50c3037916bd8200f0
Tim Quimbo <tquimbo1@gmail.com>
2021-11-20 21:51:25 -0500

    commit tree 1d46e4dd16cac8ab757615af2caa1b2b441c8c70

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+
+function mondayWork(activity="go to office"){
+    return `This Monday I will ${activity}`
+}
